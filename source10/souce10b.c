1178e9b486cf543b7d868b87cc10e1e71a460e79
Benjamim Borges <borges.benjamim@gmail.com>
2020-12-22 19:28:13 -0300

    Add files via upload

---
+++ source10/souce10b.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*
+SINTAXE do FAÇA ENQUANTO pós teste
+do { 
+    //corpo da repetição
+} while (condição);
+
+*/
+
+int main() {
+    //variaveis contadoras
+    //iniciar
+    int i = 1;
+
+    // usar na condição
+    do {
+        printf("%dEmanuel ]\n",i);
+        i++;
+    
+    } while (i<=10);
+
+    return  0;
+}
+
---
+++ source10/source 10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/*
+SINTAXE do laço ENQUANTO
+while (condição) {
+    //corpo da repetição
+}
+
+*/
+
+int main() {
+    //variaveis contadoras
+    //iniciar
+    int i = 1;
+
+    // usar na condição
+    while (i<=10){
+        printf("%dEmanuel ]\n",i);
+        i++;
+    }
+
+    return  0;
+}
+
